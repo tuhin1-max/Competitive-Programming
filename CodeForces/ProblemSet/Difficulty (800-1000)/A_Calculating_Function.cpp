b4cab51c5182d4c09bbe0e108d786ac00d8eeaf1
tuhin1-max <mdtuhinahammed2030@gmail.com>
2026-08-01 18:09:44 +0600

    Implement structural updates and optimizations across multiple modules

---
+++ CodeForces/ProblemSet/Difficulty (800-1000)/A_Calculating_Function.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    long long n;
+    cin >> n;
+    
+    long long ans;
+    if(n%2 == 0){
+        ans = n/2;
+    }
+    else{
+       ans = -(n+1)/2;
+    }
+    
+    cout << ans << endl;
+    return 0;
+}
